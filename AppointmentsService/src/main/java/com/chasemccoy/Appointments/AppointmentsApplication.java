7b1d611bcef2741611564dcd5892f81175f1c23b
Chase <=>
2023-06-30 14:23:27 -0700

    core appointment service functionality

--- AppointmentsService/src/main/java/com/chasemccoy/Appointments/AppointmentsApplication.java
+++ AppointmentsService/src/main/java/com/chasemccoy/Appointments/AppointmentsApplication.java
@@ -1,4 +1,4 @@
-package com.springDemo.Appointments;
+package com.chasemccoy.Appointments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- AppointmentsService/src/test/java/com/chasemccoy/Appointments/AppointmentsApplicationTests.java
+++ AppointmentsService/src/test/java/com/chasemccoy/Appointments/AppointmentsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springDemo.Appointments;
+package com.chasemccoy.Appointments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
