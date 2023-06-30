5b8936954fa16598d9b06432b8e6a220ea4dac35
ojasgupta-25 <57070854+ojasgupta-25@users.noreply.github.com>
2023-06-30 11:46:48 -0500

    Users Files

---
+++ Users/src/test/java/com/springDemo/Users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDemo.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
