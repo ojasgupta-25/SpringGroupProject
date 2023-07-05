d93be20dae97e1e633d3e4d016b43336fd6fe7a1
ojasgupta-25 <ojasgupta25@gmail.com>
2023-07-05 00:58:28 -0500

    Tests finished
    Could not get code coverage

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-=
-Users/pom.xml,a\c\acfb01dab0d9ed72d70a6b2c25859d0b6a80bec3
-s
-CUsers/src/test/java/com/springDemo/Users/UsersApplicationTests.java,f\6\f65ad86a09bf329865629cb321fa09c7dd55c1bd
--- Users/src/test/java/com/springDemo/Users/UsersApplicationTests.java
+++ Users/src/test/java/com/springDemo/Users/UsersApplicationTests.java
@@ -1,13 +1,75 @@
 package com.springDemo.Users;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class UsersApplicationTests {
 
+	@Autowired
+	private UserController userController;
+
 	@Test
-	void contextLoads() {
+	public void testListUsers() {
+		User user = new User();
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setGender("Male");
+		user.setAge(30);
+		user.setEmailAddress("john.doe@gmail.com");
+		user.setPhoneNumbers(Arrays.toString(new String[]{"123-456-7890", "987-654-3210"}));
+		User createdUser = userController.createUser(user);
+		List<User> users = userController.listUsers();
+		assert(users.size() > 0);
 	}
 
+	@Test
+	public void testGetUser() {
+		User user = userController.getUser(1L);
+		assert(user != null);
+	}
+
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setGender("Male");
+		user.setAge(30);
+		user.setEmailAddress("john.doe@gmail.com");
+		user.setPhoneNumbers(Arrays.toString(new String[]{"123-456-7890", "987-654-3210"}));
+		User createdUser = userController.createUser(user);
+		assert(createdUser != null);
+	}
+
+	@Test
+	public void testUpdateUser() {
+		// creating user to test update on
+		User user_ = new User();
+		user_.setFirstName("John");
+		user_.setLastName("Doe");
+		user_.setGender("Male");
+		user_.setAge(30);
+		user_.setEmailAddress("john.doe@gmail.com");
+		user_.setPhoneNumbers(Arrays.toString(new String[]{"123-456-7890", "987-654-3210"}));
+		User createdUser = userController.createUser(user_);
+
+		User user = userController.getUser(1L);
+		user.setFirstName("Jane");
+		user.setLastName("Doe");
+		user.setEmailAddress("jane.doe@gmail.com");
+		User updatedUser = userController.updateUser(1L, user);
+		assert(updatedUser != null);
+		assert(updatedUser.getFirstName().equals("Jane"));
+	}
+
+	@Test
+	public void testDeleteUser() {
+		userController.deleteUser(1L);
+		User user = userController.getUser(1L);
+		assert(user == null);
+	}
 }
